32af3dffc8131e8fe82c2c1bf73439a7083a4829
soltros <142091676+soltros@users.noreply.github.com>
2023-10-25 08:32:28 -0400

    Update configuration.nix

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -1,77 +1,83 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, ... }:
 
 {
   imports =
-    [ ./hardware-configuration.nix ];
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  nix = {
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.configurationLimit = 3;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "b450m-d3sh"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Detroit";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-  boot.loader = {
-    systemd-boot.enable = false;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
-    };
-    grub = {
-      devices = [ "nodev" ];
-      enable = true;
-      efiSupport = true;
-      useOSProber = true;
-    };
-  };
-
-  boot.kernelPackages = pkgs.linux_latest;
-
-  networking.extraHosts =
-    ''
-    10.218.1.83 ubuntu-server
-    10.218.1.98 ubuntu-backup-server
-    '';
-
-  nix.gc.automatic = true;
-  nix.gc.options = "--delete-older-than 3d";
-
-  nixpkgs.config.allowUnfree = true;
-
-  services.xserver.videoDrivers = [ "modesetting" ];
-
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
+   };   
+  
+  # Nvidia configuration
+  hardware.nvidia.nvidiaSettings = true;
+  
+  # Modesetting
+  hardware.nvidia = {
+    modesetting.enable = true;
   };
 
-  networking.hostName = "b450m-d3sh";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "America/New_York";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-  };
-
-  services.xserver.enable = true;
-
-  #virtualisation.virtualbox.host.enable = true;
-  boot.kernelParams = [ "vboxdrv.load_state=1" ];
-  boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" "vboxpci" ];
-  users.extraGroups.vboxusers.members = [ "derrik" ];
-
-  #KDE Plasma
-  services.xserver.desktopManager.plasma5.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
-  #Gnome
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
 
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -80,34 +86,100 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  services.flatpak.enable = true;
-  xdg.portal.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.derrik = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    description = "Derrik Diener";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      bitwarden
+      tdesktop
+      spotify
+      tailscale
+      home-manager
+      flatpak
+      wget
+      gimp
+      vlc
+      qbittorrent
+      wine-staging
+      pavucontrol
+      winetricks
+      element-desktop
+      nextcloud-client
+      geany
+      neofetch
+      obsidian
+      kate
+      thunderbird
+    ];
   };
 
-  environment.systemPackages = with pkgs; [
-    bitwarden mesa tdesktop python311Full python311Packages.pip thunderbird spotify discord steam tailscale papirus-icon-theme borgbackup nano home-manager wget flatpak gimp vlc quickemu qbittorrent vlc wine-staging pavucontrol winetricks flameshot element-desktop nextcloud-client git firefox-unwrapped geany gnome.gnome-disk-utility neofetch obsidian unzip killall virt-manager kate microsoft-edge yt-dlp docker distrobox vim
-  ];
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # Flatpak support
+  services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  
+  # VirtualBox support
+  virtualisation.virtualbox.host.enable = true;
+  boot.kernelParams = [ "vboxdrv.load_state=1" ];
+  boot.kernelModules = [ "vboxdrv" "vboxnetadp" "vboxnetflt" "vboxpci" ];
+  users.extraGroups.vboxusers.members = [ "derrik" ];
+  
+  # Virtualization support
+  virtualisation.libvirtd.enable = true;
 
+  # Tailscale support
   services.tailscale.enable = true;
   networking.firewall.checkReversePath = "loose";
 
-  #virtualisation.vmware.host.enable = true;
-  virtualisation.libvirtd.enable = true;
-  programs.dconf.enable = true; 
- 
-  services.openssh.enable = true;
   
-  virtualisation.docker.enable = true;
-  virtualisation.docker.storageDriver = "btrfs";
-  hardware.bluetooth.enable = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default. # wget bitwarden tdesktop spotify tailscale home-manager flatpak gimp vlc qbittorrent wine-staging pavucontrol winetricks element-desktop nextcloud-client geany neofetch obsidian
+  ];
 
-# NixOS version
-  system.stateVersion = "23.05";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
 }
