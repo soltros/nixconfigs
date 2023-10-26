e64c98418e70879f661e8995b550e5dc22dffad2
soltros <142091676+soltros@users.noreply.github.com>
2023-10-25 21:30:46 -0400

    Update configuration.nix

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -201,7 +201,23 @@
   # Tailscale support
   services.tailscale.enable = true;
   networking.firewall.checkReversePath = "loose";
+   
+  # Podman support
+  virtualisation = {
+    podman = {
+      enable = true;
 
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+      # For Nixos version > 22.11
+      #defaultNetwork.settings = {
+      #  dns_enabled = true;
+      #};
+    };
+  };
   
 
   # List packages installed in system profile. To search, run:
