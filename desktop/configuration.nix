b828b2378a407ddad1400a9f20fd7563b6c7d141
soltros <142091676+soltros@users.noreply.github.com>
2023-11-12 18:55:01 -0500

    Update configuration.nix

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -11,6 +11,7 @@
     ];
 
   # Bootloader.
+  boot.loader.systemd-boot.enable = false;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
