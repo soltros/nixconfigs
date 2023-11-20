605d172e0f499175dbc29097767795d98be0fe04
soltros <142091676+soltros@users.noreply.github.com>
2023-11-20 05:54:05 -0500

    Create wallpaper-downloader.sh

---
+++ wallpaper-downloader.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/Pictures/
+
+mkdir -p ~/Pictures/Wallpaper/
+
+git clone https://github.com/soltros/chromecast-images-archive.git
+
+cd chromecast-images-archive/
+
+mv *.* ~/Pictures/Wallpaper/
