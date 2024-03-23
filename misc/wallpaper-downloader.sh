238414efbf5bdfc665d262592dc0acbace3340b3
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-22 23:33:08 -0400

    Create wallpaper-downloader.sh

---
+++ misc/wallpaper-downloader.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/Pictures/
+
+mkdir -p ~/Pictures/Wallpaper/
+
+git clone https://github.com/soltros/chromecast-images-archive.git
+
+cd chromecast-images-archive/
+
+mv *.* ~/Pictures/Wallpaper/
+
+rm -rf ~/Pictures/chromecast-images-archive/
