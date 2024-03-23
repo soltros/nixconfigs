42a61bbaa534de4df98222c4443aabad83ce47d9
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-03-23 01:37:00 -0400

    Add files via upload

---
+++ misc/environment-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.config/fish/
+cd ~/.config/fish/
+wget https://raw.githubusercontent.com/soltros/nixconfigs/main/misc/config.fish
+cd ~/
+wget https://raw.githubusercontent.com/soltros/nixconfigs/main/misc/.bashrc
