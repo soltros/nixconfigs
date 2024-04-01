725ee5308482c91908a7cf6e1a2105cf1b106256
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-04-01 16:32:18 -0400

    Update and rename environment-deploy.sh to environment-deploy-desktop.sh

--- misc/environment-deploy-desktop.sh
+++ misc/environment-deploy-desktop.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.config/fish/
 cd ~/.config/fish/
-wget https://raw.githubusercontent.com/soltros/nixconfigs/main/misc/config.fish
+rm config.fish
+wget https://raw.githubusercontent.com/soltros/nixconfigs/main/misc/config.fish.desktop -O config.fish
 cd ~/
 wget https://raw.githubusercontent.com/soltros/nixconfigs/main/misc/.bashrc
