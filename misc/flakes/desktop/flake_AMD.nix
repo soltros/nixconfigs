2e11f58c3da5ac7c10cb775c9cb49cd6b7b18bdb
Derrik Diener <142091676+soltros@users.noreply.github.com>
2024-11-20 19:23:21 -0500

    Create flake_AMD.nix

---
+++ misc/flakes/desktop/flake_AMD.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-software-center.url = "github:snowfallorg/nix-software-center";
+    hardware.url = "github:nixos/nixos-hardware";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, home-manager, hardware, flake-utils, nixos-cosmic, nix-software-center, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.b450m-d3sh = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.derrik = import ./home.nix;
+          }
+          nixos-cosmic.nixosModules.default
+          {
+            system.stateVersion = "24.11";
+            environment.systemPackages = [
+              nix-software-center.packages.${system}.nix-software-center
+            ];
+            nix = {
+              settings = {
+                experimental-features = [ "nix-command" "flakes" ];
+                auto-optimise-store = true;
+                substituters = [ "https://cosmic.cachix.org/" ];
+                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+              };
+              gc = {
+                automatic = true;
+                dates = "weekly";
+                options = "--delete-older-than 30d";
+              };
+            };
+          }
+        ];
+      };
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil
+          alejandra
+          statix
+        ];
+      };
+    };
+}
