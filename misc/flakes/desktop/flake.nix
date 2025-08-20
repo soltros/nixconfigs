c18def762ab7359fa1411c0054bb03f914a3c7ee
Derrik Diener <142091676+soltros@users.noreply.github.com>
2025-08-20 03:26:25 -0400

    Update flake.nix

--- misc/flakes/desktop/flake.nix
+++ misc/flakes/desktop/flake.nix
@@ -1,35 +1,48 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    hyprland.url = "github:hyprwm/Hyprland";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-software-center.url = "github:snowfallorg/nix-software-center";
     hardware.url = "github:nixos/nixos-hardware";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, hardware, flake-utils, nixos-cosmic, nix-software-center, ... }:
+  outputs = { self, nixpkgs, home-manager, hardware, flake-utils, hyprland, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations.b450m-d3sh = nixpkgs.lib.nixosSystem {
         inherit system;
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
-          nixos-cosmic.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.derrik = import ./home.nix;
+          }
           {
             system.stateVersion = "25.05";
-            environment.systemPackages = [
-              nix-software-center.packages.${system}.nix-software-center
-            ];
+            
+            # Enable Hyprland with latest version from flake
+            programs.hyprland = {
+              enable = true;
+              package = hyprland.packages.${system}.hyprland;
+              portalPackage = hyprland.packages.${system}.xdg-desktop-portal-hyprland;
+            };
+            
             nix = {
               settings = {
                 experimental-features = [ "nix-command" "flakes" ];
                 auto-optimise-store = true;
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                # Add Hyprland cachix to avoid recompilation
+                substituters = [ "https://hyprland.cachix.org" ];
+                trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
               };
               gc = {
                 automatic = true;
@@ -40,7 +53,6 @@
           }
         ];
       };
-
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           nil
